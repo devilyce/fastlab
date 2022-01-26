2ddd4ec5bbf2baa641923e9332875a5bffc967ce
den silverio <dennsilverio@gmail.com>
2022-01-26 17:09:16 +0800

    email fix

--- fastlab/settings.py
+++ fastlab/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-%4($ubthjcchld1f16_)p35a6e=$!+j6^c896=kof4nusjh53k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['testserver.fastlab.ph', 'www.testserver.fastlab.ph', '127.0.0.1:8000', '127.0.0.1' ]
+ALLOWED_HOSTS = ['testserver.fastlab.ph', 'www.testserver.fastlab.ph', '127.0.0.1:8000', '127.0.0.1']
 
 # Application definition
 
@@ -118,7 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -132,12 +133,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # SMTP Configuration
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-DEFAULT_FROM_EMAIL = 'fastlabtest@gmail.com'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'fastlabtest@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'fastlabtestmail@gmail.com'
+EMAIL_HOST_PASSWORD = 'denski16'
+
 EMAIL_FILE_PATH = '/media/'
