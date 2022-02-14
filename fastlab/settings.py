abc69af67cb37c241caadb3023c89666e16ee85b
den silverio <dennsilverio@gmail.com>
2022-02-14 20:12:41 +0800

    email fix

--- fastlab/settings.py
+++ fastlab/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -134,10 +134,11 @@ MEDIA_URL = '/media/'
 
 # SMTP Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
+EMAIL_HOST = 'sgpro4.fcomet.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'fastlabtestmail@gmail.com'
-EMAIL_HOST_PASSWORD = 'denski16'
+EMAIL_USE_TLS = False
+EMAIL_HOST_USER = 'test@fastlab.ph'
+EMAIL_HOST_PASSWORD = 'Fastlab2020!!'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 EMAIL_FILE_PATH = '/media/'
